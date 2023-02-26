1b10a5fcabac52d575446d03e6983126afe3f801
GlezovDmitriy <dmitriy_glezov@mail.ru>
2023-02-26 22:15:48 +0300

    Task 15

--- index.js
+++ index.js
@@ -110,3 +110,13 @@ function strCount(str, letter){
 }
 
 //15
+function numberToPower(number, power){
+  console.info(Math.log2(1024));
+  let result=1;
+  for (let i=1; i<= power; i++){
+    result = result*number;
+  }
+  return result;
+}
+
+//16
