681bf9df8611beadb97e3f01463509eb9736656f
GlezovDmitriy <dmitriy_glezov@mail.ru>
2023-03-02 22:21:58 +0300

    Task 18

--- index.js
+++ index.js
@@ -139,3 +139,17 @@ var humanYearsCatYearsDogYears = function(humanYears) {
 }
 
 //17
+
+function countSheeps(arrayOfSheep) {
+  let sum =0;
+  
+  for (let i = 0; i<=arrayOfSheep.length; i++){
+    if (arrayOfSheep[i]==true && arrayOfSheep[i]!=null && arrayOfSheep[i] !=undefined){
+       sum+=1;
+    } else continue;
+  }
+  return sum;
+}
+
+//18
+
