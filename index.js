ab9d0c0e873822ea26ffca915f0376016def5427
GlezovDmitriy <dmitriy_glezov@mail.ru>
2023-03-05 11:21:57 +0300

    Task 33

--- index.js
+++ index.js
@@ -290,3 +290,13 @@ function powersOfTwo(n){
   return arr;
 }
 
+//33 Напишите программу, которая находит сумму всех чисел от 1 до num. Число всегда будет положительным целым числом больше 0.
+
+var summation = function (num) {
+  let sum = 0;
+ for (let i= 1; i<=num; i++){
+   sum +=i;
+ }
+  return sum;
+}
+
