9d92f5b82589f73c0af1e46ff7d49aa3cf436e51
GlezovDmitriy <dmitriy_glezov@mail.ru>
2023-03-12 16:12:21 +0300

    task 53

--- 50+.js
+++ 50+.js
@@ -7,4 +7,15 @@ function joinStrings(string1, string2){
     return `${string1} ${string2}`;// code here
  }
  
-//53
+//53 если переданы параметры (2, 6), функция должна возвращать [2, 4, 6] значения 2, 4 и 6, кратные от 2 до 6. если(5,17) то [5,10,15]
+
+
+function findMultiples(integer, limit) {
+    let arr = [];
+    let x;
+    for (let i = integer; i<= limit; i+=integer){
+      arr.push(i);
+        } ;
+    return arr;
+  }
+  
