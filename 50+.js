1d1f5709a72cd658cdac8dfee9e416b5ba6bf9dd
GlezovDmitriy <dmitriy_glezov@mail.ru>
2023-03-12 16:23:31 +0300

    task 54

--- 50+.js
+++ 50+.js
@@ -9,7 +9,6 @@ function joinStrings(string1, string2){
  
 //53 если переданы параметры (2, 6), функция должна возвращать [2, 4, 6] значения 2, 4 и 6, кратные от 2 до 6. если(5,17) то [5,10,15]
 
-
 function findMultiples(integer, limit) {
     let arr = [];
     let x;
@@ -18,4 +17,10 @@ function findMultiples(integer, limit) {
         } ;
     return arr;
   }
-  
+ 
+//54 строку в число(в строке цифры)
+
+const stringToNumber = function(str){
+    // put your code here
+    return str*1;
+  }
