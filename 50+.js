077773e9c40f5eeaddb9b8b12e7a8c93c18bc7d8
GlezovDmitriy <dmitriy_glezov@mail.ru>
2023-03-16 19:59:58 +0300

    task 64

--- 50+.js
+++ 50+.js
@@ -123,3 +123,12 @@ function addFive(num) {
 }
 
 //64
+
+var laLigaGoals = 43;
+var championsLeagueGoals = 10;
+var copaDelReyGoals = 5
+
+var totalGoals = laLigaGoals + championsLeagueGoals + copaDelReyGoals;
+
+//65
+
