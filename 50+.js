f77b9176ec6a7dc1c46e0b52ee86deb90c4c4125
GlezovDmitriy <dmitriy_glezov@mail.ru>
2023-03-20 21:12:02 +0300

    task 70

--- 50+.js
+++ 50+.js
@@ -208,4 +208,13 @@ welsh: 'Croeso'
 
 //70
 
+function areYouPlayingBanjo(name) {
+  if (name[0] == 'R' || name[0] == 'r'){
+    return `${name} plays banjo`;
+  }
+  return `${name} does not play banjo`;
+}
+
+//71
+
 
