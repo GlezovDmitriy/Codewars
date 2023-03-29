366ddc8703df32f3f05ec180a0fcea4c2b0698a7
GlezovDmitriy <dmitriy_glezov@mail.ru>
2023-03-29 19:53:50 +0300

    Task 88

--- 50+.js
+++ 50+.js
@@ -422,6 +422,15 @@ function sortMyString(S) {
    return str1 +' '+ str2;
 }
 
+//88
+
+function getRealFloor(n) {
+  if (n>=0 && n<13){
+    return n-1;
+  } else if (n >=13){
+    return n-2;
+  } else return n;
+}
 
 
 
