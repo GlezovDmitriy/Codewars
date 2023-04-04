df3c3a0b9564df77a4f49807e40bf1f74f4c2860
GlezovDmitriy <dmitriy_glezov@mail.ru>
2023-04-04 21:34:30 +0300

    refactored task 100

--- 50+.js
+++ 50+.js
@@ -559,7 +559,20 @@ function betterThanAverage(classPoints, yourPoints) {
   else return false;
 }
 
+//100
 
+function evalObject(value){
+  var result = 0;
+  switch(value.operation){
+    case'+': result = value.a + value.b; break;
+    case'-': result = value.a - value.b; break;
+    case'/': result = value.a / value.b; break;
+    case'*': result = value.a * value.b; break;
+    case'%': result = value.a % value.b; break;
+    case'^': result = Math.pow(value.a, value.b); break;
+  }
+  return result;
+}
 
 
 
