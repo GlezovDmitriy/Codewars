b13bc1d90ac7928bf1a7840086a799baa112eb1e
GlezovDmitriy <dmitriy_glezov@mail.ru>
2023-04-05 22:24:44 +0300

    task 114

--- 100+.js
+++ 100+.js
@@ -111,3 +111,10 @@ function removeEveryOther(arr){
   }
   return arr2;
 }
+
+//114
+
+function pillars(numPill, dist, width) {
+  if (numPill == 1) return 0;
+  else return ((numPill-1) * dist)*100 + ((numPill-2) * width);
+}
