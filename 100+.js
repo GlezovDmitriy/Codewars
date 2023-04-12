fdb92b8f8ec5aa0c3b5f00794c959c91b9ece4db
GlezovDmitriy <dmitriy_glezov@mail.ru>
2023-04-12 23:45:49 +0300

    task 123

--- 100+.js
+++ 100+.js
@@ -195,3 +195,15 @@ function twiceAsOld(dad, son) {
   let years = dad - 2*son;
   return Math.abs(years);
 }
+
+//123
+
+function maps(x){
+  let arr = [];
+    for (let i =0; i<x.length; i++){
+      arr.push(x[i]*2);
+    }
+    return arr;
+  }
+
+ //124 
