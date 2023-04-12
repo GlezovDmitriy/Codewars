38aad637a337d1ffb98f240079be86cb340bc919
GlezovDmitriy <dmitriy_glezov@mail.ru>
2023-04-12 23:57:20 +0300

    task 124

--- 100+.js
+++ 100+.js
@@ -207,3 +207,27 @@ function maps(x){
   }
 
  //124 
+
+ function getPlanetName(id){
+  let name;
+  switch(id){
+    case 1:
+      name = 'Mercury'; break;
+    case 2:
+      name = 'Venus'; break;
+    case 3:
+      name = 'Earth'; break;
+    case 4:
+      name = 'Mars'; break;
+    case 5:
+      name = 'Jupiter'; break;
+    case 6:
+      name = 'Saturn'; break;
+    case 7:
+      name = 'Uranus'; break;
+    case 8:
+      name = 'Neptune'; break;
+  }
+  
+  return name;
+} 
