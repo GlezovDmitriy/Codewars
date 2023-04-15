5f60114f9556d9be916e92aec5c9cdb9b3db35cb
GlezovDmitriy <dmitriy_glezov@mail.ru>
2023-04-15 23:45:01 +0300

    task 139

--- 100+.js
+++ 100+.js
@@ -349,3 +349,16 @@ function solution(a, b){
     return b+a+b;
   } else return a+b+a;
 }
+
+//139  массив чисел, возвращаете сумму всех положительных.
+
+function positiveSum(arr) {
+  if(arr.length == 0) return 0;
+  let sum = 0;
+  for(let i = 0; i<arr.length; i++){
+    if(arr[i] > 0){
+      sum += arr[i];
+    } else continue;
+  }
+  return sum;
+}
