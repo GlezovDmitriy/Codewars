d75179607cd583cde2b7b3bd5861357151247724
GlezovDmitriy <dmitriy_glezov@mail.ru>
2023-04-19 23:18:49 +0300

    task 149

--- 100+.js
+++ 100+.js
@@ -446,3 +446,14 @@ function correctTail(body, tail) {
 
 //149
 
+function fakeBin(x){
+  let str = '';
+    for( let i = 0; i<x.length; i++){
+      if(x[i]<5){
+        str += 0;
+      } else str += 1;
+    }
+    return str;
+  }
+
+  
