6e4d1373e5c450a127f4e242d8b45214054d5b2c
GlezovDmitriy <dmitriy_glezov@mail.ru>
2023-04-22 22:27:39 +0300

    task 155

--- 150+.js
+++ 150+.js
@@ -38,3 +38,21 @@ function playerRankUp (points) {
   }
 
 //155  
+
+const rps = (p1, p2) => {
+    if (p1 == p2) return "Draw!";
+    else if (p1 == "scissors" && p2 =="paper")
+      return "Player 1 won!";
+    else if (p2 == "scissors" && p1 =="paper")
+      return "Player 2 won!";
+    else if (p1 == "scissors" && p2 =="rock")
+      return "Player 2 won!";
+    else if (p2 == "scissors" && p1 =="rock")
+      return "Player 1 won!";
+    else if (p1 == "paper" && p2 =="rock")
+      return "Player 1 won!";
+    else if (p2 == "paper" && p1 =="rock")
+      return "Player 2 won!";
+  };
+
+  
