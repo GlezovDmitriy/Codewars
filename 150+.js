66d08c0dffaabcc380699245b5026f51bf6d1a66
GlezovDmitriy <dmitriy_glezov@mail.ru>
2023-04-28 14:46:59 +0300

    task 171

--- 150+.js
+++ 150+.js
@@ -208,3 +208,14 @@ function sumMul(n,m){
   }
  return sum;
 }
+
+//171
+
+function peopleWithAgeDrink(old) {
+  if(old <14 ) return "drink toddy";
+  else if (old <18 ) return "drink coke";
+  else if (old <21 ) return "drink beer";
+  else return "drink whisky";
+};
+
+
