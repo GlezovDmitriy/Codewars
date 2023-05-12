c883b66273c8ff1a2798acf12afc2612c005ceb7
GlezovDmitriy <dmitriy_glezov@mail.ru>
2023-05-12 22:42:50 +0300

    task 188

--- 150+.js
+++ 150+.js
@@ -416,5 +416,15 @@ function tripleTrouble(one, two, three){
       return "I'd like to take this chance to apologize.. To absolutely NOBODY!";
   };  
 
+//188
+
+function rainAmount(mm){
+  if (mm >= 40) {
+       return "Your plant has had more than enough water for today!"
+  }else {
+       return `You need to give your plant ${40 - mm}mm of water`
+  };
+}
+
 
 
