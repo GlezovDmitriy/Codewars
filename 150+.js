54f7d22c571ab672a158d7810c6e533907df7504
GlezovDmitriy <dmitriy_glezov@mail.ru>
2023-05-14 22:01:44 +0300

    task 189

--- 150+.js
+++ 150+.js
@@ -426,5 +426,13 @@ function rainAmount(mm){
   };
 }
 
+//189
 
+function Sleigh() {}
+
+Sleigh.prototype.authenticate = function(name, password) {
+  if (name == "Santa Claus" && password == "Ho Ho Ho!"){
+    return true;
+  } else return false;
+};
 
