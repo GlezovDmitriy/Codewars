4ee1f25b8311747453818a2120d2892066cd7c9d
GlezovDmitriy <dmitriy_glezov@mail.ru>
2023-06-20 22:09:15 +0300

    task 201

--- 200+.js
+++ 200+.js
@@ -5,4 +5,13 @@ solution= (molarMass1, molarMass2, givenMass1, givenMass2, volume, temp) => {
     return ((givenMass1/molarMass1 + givenMass2/molarMass2) * 0.082 * (temp+273.15)) / volume;
     }
 //201
-    
+   
+function getSize(width, height, depth){
+    let area = width*height*2 + width*2*depth + height*2*depth;
+    let volume = width * height * depth;
+    return [ area, volume];
+  }
+
+ //202
+ 
+ 
