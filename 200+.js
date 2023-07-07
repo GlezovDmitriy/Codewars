12b7fc49d41916e8531fca81db85a0ee890e79e7
GlezovDmitriy <dmitriy_glezov@mail.ru>
2023-07-07 21:25:50 +0300

    task 230

--- 200+.js
+++ 200+.js
@@ -265,3 +265,15 @@ var findAverage = function (nums) {
 function twoDecimalPlaces(n) {
   return n.toFixed(2)*1;
   }
+
+//230
+
+function leo(oscar){
+  if (oscar == 88) return  "Leo finally won the oscar! Leo is happy";
+  else if (oscar == 86) return "Not even for Wolf of wallstreet?!";
+  else if ( oscar == 87 || oscar < 86) return "When will you give Leo an Oscar?";
+  else return  "Leo got one already!";
+}
+
+//231
+
