8b82b4204a93820eb2411c0ac61b63e8a23a6d5e
GlezovDmitriy <dmitriy_glezov@mail.ru>
2023-07-10 20:39:39 +0300

    task 231

--- 200+.js
+++ 200+.js
@@ -277,3 +277,11 @@ function leo(oscar){
 
 //231
 
+let name = 'Abe';
+let greet_abe = function() {
+  return "Hello, " + name + '!';
+};
+let name2 = 'Ben';
+let greet_ben = function() {
+  return "Hello, " + name2 + '!';
+};
