1bf93e2456fbc6d3dd0f76dd1652b217a62b355c
GlezovDmitriy <dmitriy_glezov@mail.ru>
2023-07-17 21:17:14 +0300

    task 248

--- 200+.js
+++ 200+.js
@@ -494,3 +494,9 @@ function reverseWords(str) {
   return arr.join(" ");
 }
 
+//248
+
+function arithmetic(a, b, operator){
+  return operator == 'add'? a+b: operator == 'subtract'? a-b: operator == 'multiply'? a*b : a/b;
+}
+
