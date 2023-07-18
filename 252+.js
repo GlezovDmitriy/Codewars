1e2df1f6694203baf41ab416e9150b941ac09490
GlezovDmitriy <dmitriy_glezov@mail.ru>
2023-07-18 23:26:41 +0300

    task 253

--- 252+.js
+++ 252+.js
@@ -3,3 +3,14 @@
 function createPhoneNumber(n){
     return "("+`${n[0]}`+`${n[1]}`+`${n[2]}`+")"+" "+`${n[3]}`+`${n[4]}`+`${n[5]}`+"-"+`${n[6]}`+`${n[7]}`+`${n[8]}`+`${n[9]}`;
   }
+
+//253  Дан список целых чисел, определить, является ли сумма его элементов четной или нечетной.
+
+function oddOrEven(array) {
+  let sum =0;
+  array.forEach(function(el){
+    sum+=el
+  });
+  if (sum%2 == 0) return "even";
+  else return 'odd';
+}
