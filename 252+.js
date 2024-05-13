aabaf7758c081a6b027897889da62806519ff031
GlezovDmitriy <dmitriy_glezov@mail.ru>
2024-05-13 16:26:59 +0300

    Added 287 task

--- 252+.js
+++ 252+.js
@@ -437,3 +437,12 @@ function isIsogram(str){
   }
   return true;
 }
+
+//287 Are the numbers in order?
+function inAscOrder(arr) {
+  for (let i=0; i<arr.length-1; i++){
+    if (arr[i] < arr[i+1]) continue
+    else return false
+  }
+  return true
+}
--- index.js
+++ index.js
@@ -1,32 +1,10 @@
-const f = (str) => {
-   /* str.toLowerCase();
-    let arr = str.split('').sort();
-    console.log(arr)
 
-    for (let i = 0; i < arr.length-1; i++) {
-        for (let j = 1; j < arr.length; j++) {
-            if (arr[i] === arr[j])
-                return 'false';
-
-        }
-
+function inAscOrder(arr) {
+    for (let i=0; i<arr.length-1; i++){
+        if (arr[i] < arr[i+1]) continue
+        else return false
     }
-    return 'true';*/
-    const letters = {};
-    console.log(letters)
-    for (let i = 0; i < str.length; i++) {
-        const letter = str[i].toLowerCase();
-
-        if (letters[letter]) {
-            return false;
-        }
-
-        letters[letter] = true;
-    }
-
-    return true;
-
+    return true
 }
-//console.log(f("mirrors"))
-console.log(f("misss"))
-console.log(f("mis"))
+
+console.log(inAscOrder([1, 2, 4, 7, 19]))
